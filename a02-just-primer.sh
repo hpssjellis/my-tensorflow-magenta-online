56d2683dbf367d890362c6abd70a1a3831ae75de
Jeremy Ellis <keyfreemusic@gmail.com>
2016-09-19 17:30:56 -0700

    Update a02-just-primer.sh

--- a02-just-primer.sh
+++ a02-just-primer.sh
@@ -1,25 +1,35 @@
-#!/bin/bash
+#----------------------basic_rnn_generate----------------------------------------------------
+
+#basic_rnn_generate Generates the output midi files
+#--run_dir=location of the latest run and checkpoints
+#--hparams=comma seperated list of hparameters
+#--output_dir Directory for all your midi files to be generated into
+#--num_steps Number of notes generated, more notes the longer the song is
+#--num_outputs Number of midi files generated
+#--temperature Randomness of outputted song. Default = 1, higher numbers for more random songs
+#--bpm  Beats per minute for the output songs. Default 120 bpm
+#--log-INFO is the default.Others in order are  DEBUG, INFO, WARN, ERROR, or FATAL 
+#--primer_midi A small midi file that starts each outputted midi fie
+#or
+# --primer_melody Strange format for writing small midi style songs
+#0r
+#leave blank for the first note to be randomly generated
 
 
 
-
-# Provide a MIDI file to use as a primer for the generation.
-# The MIDI should just contain a short monophonic melody.
-# primer.mid is provided as an example.
-PRIMER_PATH=~/mymagenta/magenta/magenta/models/basic_rnn/primer.mid
-
-bazel run //magenta/models:basic_rnn_generate -- \
---experiment_run_dir=/tmp/basic_rnn/run1 \
---hparams='{"rnn_layer_sizes":[50]}' \
---primer_midi=$PRIMER_PATH \
+bazel run //magenta/models/basic_rnn:basic_rnn_generate -- \
+--run_dir=/tmp/basic_rnn/logdir/run1 \
+--hparams=$HPARAMS_SET \
 --output_dir=/tmp/basic_rnn_generated \
---num_steps=650 \
---num_outputs=1
+--num_steps=640 \
+--num_outputs=1 \
+--temperature=1 \
+--bpm=30 \
+--log=DEBUG \
+--primer_melody="[60, -2, 60, -2, 67, -2, 67, -2]"
 
 
-echo "install a midi player like timidity"
-echo ""
-echo "play it using the command line"
-echo "timidity basic_run_sample_0.mid"
-echo "or copy it to an online midi site such as https://onlinesequencer.net/"
+#\
+#--primer_midi=~/mymagenta/magenta/magenta/models/shared/primer.mid
 
+# --primer_melody="[60, -2, 60, -2, 67, -2, 67, -2]"
