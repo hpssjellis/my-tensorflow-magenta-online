da64ae8d7e7180578b4c665338125fd938507692
Jeremy Ellis <keyfreemusic@gmail.com>
2016-09-20 22:00:39 -0700

    Update a05-pre-trained-by-magenta.sh

--- a05-pre-trained-by-magenta.sh
+++ a05-pre-trained-by-magenta.sh
@@ -3,11 +3,11 @@
 
 
 
-BUNDLE_PATH=<absolute path of basic_rnn.mag>
+BUNDLE_PATH=/home/ubuntu/workspace/magenta/trained-magenta/basic_rnn.mag
 
 bazel run //magenta/models/basic_rnn:basic_rnn_generate -- \
 --bundle_file=${BUNDLE_PATH} \
 --output_dir=/tmp/basic_rnn/generated \
---num_outputs=10 \
---num_steps=128 \
+--num_outputs=1 \
+--num_steps=640 \
 --primer_melody="[60]"
